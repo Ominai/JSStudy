e9b71de766031917e386bdad640cfd04893e87c0
Ominai <104097942+Ominai@users.noreply.github.com>
2024-11-22 19:31:00 -0800

    Added 1-6 and 1-7

--- codecademy/1-4.js
+++ codecademy/1-4.js
@@ -1,10 +1,7 @@
 /*
 Basic arithmetic often comes in handy when programming.
 
-An 
-Preview: Docs Loading link description
-operator
- is a character that performs a task in our code. JavaScript has several built-in arithmetic operators, that allow us to perform mathematical calculations on numbers. These include the following operators and their corresponding symbols:
+operator - is a character that performs a task in our code. JavaScript has several built-in arithmetic operators, that allow us to perform mathematical calculations on numbers. These include the following operators and their corresponding symbols:
 
 Add: +
 Subtract: -
@@ -19,14 +16,11 @@ console.log(5 - 1); // Prints 4
 console.log(4 * 2); // Prints 8
 console.log(9 / 3); // Prints 3
 
-to Clipboard
-
 Note that when we console.log() the computer will evaluate the expression inside the parentheses and print that result to the console. If we wanted to print the characters 3 + 4, we would wrap them in quotes and print them as a string.
 
 console.log(11 % 3); // Prints 2
 console.log(12 % 3); // Prints 0
 
-to Clipboard
 
 The remainder operator, sometimes called modulo, returns the number that remains after the right-hand number divides into the left-hand number as many times as it evenly can: 11 % 3 equals 2 because 3 fits into 11 three times, leaving 2 as the remainder.
 */
