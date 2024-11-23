171a10bb68eed16fd8f7ac89fc9e9e4d802c91a8
Ominai <104097942+Ominai@users.noreply.github.com>
2024-11-22 22:50:31 -0800

    Consolidated Intro

--- codecademy/Intro.js
+++ codecademy/Intro.js
@@ -107,7 +107,6 @@ console.log('Teaching the world how to code'.length);
 
 /*
 Remember that methods are actions we can perform. Data types have access to specific methods that allow us to handle instances of that data type. JavaScript provides a number of string methods.
-
 We call, or use, these methods by appending an instance with:
 
 a period (the dot operator)
@@ -124,8 +123,6 @@ Let’s look at each of the lines above:
 
 On the first line, the .toUpperCase() method is called on the string instance 'hello'. The result is logged to the console. This method returns a string in all capital letters: 'HELLO'.
 On the second line, the .startsWith() method is called on the string instance 'Hey'. This method also accepts the character 'H' as an input, or argument, between the parentheses. Since the string 'Hey' does start with the letter 'H', the method returns the boolean true.
-
-You can find a list of built-in string methods in the JavaScript documentation. Developers use documentation as a reference tool. It describes JavaScript’s keywords, methods, and syntax.
 */
 
 // Use .toUpperCase() to log 'Codecademy' in all uppercase letters
